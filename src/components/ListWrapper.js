2b70190e3b044526895eab7cab5f14da6cd7f6de
Kuba <kubahermyt@gmail.com>
2020-07-23 08:23:53 +0200

    feat: created parent component with initial state

--- src/components/ListWrapper.js
+++ src/components/ListWrapper.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ListItem from "./ListWrapper/ListItem/ListItem";
 import "./ListWrapper.css";
-import { twitterAccounts } from "../data/twitterAccounts";
 
-const ListWrapper = () => (
+const ListWrapper = (props) => (
   <ul className="list">
-    {twitterAccounts.map((item) => (
+    {props.items.map((item) => (
       <ListItem key={item.twitterLink} {...item} />
     ))}
   </ul>
