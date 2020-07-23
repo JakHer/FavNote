629b550ac70683f66c610a1f40ecd2316cc67fda
Kuba <kubahermyt@gmail.com>
2020-07-23 07:25:58 +0200

    feat: first state change

--- src/components/MyComponent/MyComponent.js
+++ src/components/MyComponent/MyComponent.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    text: "test state",
+  };
+  handleChange = () => console.log(this.state.text);
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <>
+        <h1>{this.state.text}</h1>
+        <input placeholder="Enter text" onChange={this.handleChange} />
+      </>
+    );
   }
 }
 
