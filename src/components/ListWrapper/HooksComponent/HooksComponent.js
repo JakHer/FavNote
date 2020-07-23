7803ad8f95c378f2bccac6cbcb68176bf1f286b5
Kuba <kubahermyt@gmail.com>
2020-07-23 08:05:29 +0200

    feat: rewrote class component to functional component with hooks and useState

--- src/components/ListWrapper/HooksComponent/HooksComponent.js
+++ src/components/ListWrapper/HooksComponent/HooksComponent.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
 
 const HooksComponent = () => {
-  const [name, setName] = useState("Kuba");
-  const [surname, setSurname] = useState("Hermyt");
-
-  const resetPerson = (e) => {
-    e.preventDefault();
-
-    setName("Name");
-    setSurname("Surname");
-  };
+  const [text, setText] = useState("Test");
 
   return (
     <>
-      <form onSubmit={resetPerson}>
-        <input onChange={(e) => setName(e.target.value)} value={name} />
-        <input onChange={(e) => setSurname(e.target.value)} value={surname} />
-        <button type="submit">Submit</button>
-      </form>
-      <span>
-        {name} {surname}
-      </span>
+      <h1>{text}</h1>
+      <input
+        placeholder="Enter text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
     </>
   );
 };
