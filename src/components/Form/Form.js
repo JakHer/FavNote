4705c426b764e0c01350475d00b2249eee923b5e
Kuba <kubahermyt@gmail.com>
2020-07-26 09:47:45 +0200

    feat: added ternary operator for form heading

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,7 +6,7 @@ import styles from "./Form.module.scss";
 
 const Form = ({ submitFn }) => (
   <div className={styles.form__wrapper}>
-    <Heading>Add new twitter account</Heading>
+    <Heading form>Add new twitter account</Heading>
     <form className={styles.form} autoComplete="off" onSubmit={submitFn}>
       <Input name="name" label="Name" maxLength={30} />
       <Input name="link" label="Twitter Link" />
--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Heading.module.scss";
 
-const Heading = ({ children }) => (
-  <h2 className={styles.heading}>{children}</h2>
+const Heading = ({ children, form }) => (
+  <>
+    {form ? (
+      <h2 className={styles.heading__form}>{children}</h2>
+    ) : (
+      <h2 className={styles.heading}>{children}</h2>
+    )}
+  </>
 );
 
 export default Heading;
