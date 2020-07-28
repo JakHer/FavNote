5534bfb679b9d46d2221a47b35a432be5840e151
Kuba <kubahermyt@gmail.com>
2020-07-28 12:31:52 +0200

    feat: list fixed, now only twitters have image and button fix

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -4,30 +4,32 @@ import Button from "../Button/Button";
 import Heading from "../Heading/Heading.js";
 import styles from "./ListItem.module.scss";
 
-const ListItem = ({ title, description, image, link }) => {
+const ListItem = ({ title, description, image, link, type }) => {
   const ImageTag = image ? "img" : "div";
 
   return (
     <li className={styles.list__item}>
-      {image ? (
-        <ImageTag className={styles.list__image} alt={title} src={image} />
-      ) : (
-        <ImageTag className={styles.list__imageNone} alt={title} />
-      )}
+      {type === "twitter" ? (
+        image ? (
+          <ImageTag className={styles.list__image} alt={title} src={image} />
+        ) : (
+          <ImageTag className={styles.list__imageNone} alt={title} />
+        )
+      ) : null}
 
       <div className={styles.list__container}>
         <Heading>{title}</Heading>
         <p className={styles.list__description}>{description}</p>
-        {link && (
+        {type === "twitter" || type === "article" ? (
           <Button
             target="_blank"
             rel="noopener noreferrer"
             className={styles.list__link}
             href={link}
           >
-            {image ? `visit twitter page` : `visit article page`}
+            visit {type} page
           </Button>
-        )}
+        ) : null}
       </div>
     </li>
   );
